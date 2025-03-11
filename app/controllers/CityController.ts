4b0cbfc718e807c78846958a66a1f49a723cc218
ZUHOWKS <jojo.vilard38@gmail.com>
2025-03-11 16:49:09 +0100

    add controller & routes

---
+++ app/controllers/CityController.ts
@@ -0,0 +1,37 @@
+import {HttpContext} from "@adonisjs/core/http";
+import City from "#models/city";
+
+
+export default class CityController {
+
+  async getCity({request, response}: HttpContext) {
+
+    const id: number = parseInt(request.param('id'))
+
+    const city = await City.findOrFail(id)
+
+    response.json(city)
+  }
+
+  async createCity({request, response}: HttpContext) {
+    const {department_code, insee_code, name, zip_code, lat, lon} = request.body();
+
+    console.log(request.body())
+
+    const city = await City.create({
+      department_code: department_code,
+      insee_code: insee_code,
+      name: name,
+      zip_code: zip_code,
+      lat: lat,
+      lon: lon
+    })
+
+    if (city) {
+      return response.json(city)
+    } else {
+      return response.status(500).json({message: 'Error while creating city'})
+    }
+
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,17 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import CityController from "#controllers/CityController";
 
 router.on('/').render('pages/home')
+
+router.group(() => {
+
+  router.post('/city', [CityController, 'createCity'])
+
+  router.group(() => {
+    router.get('/:id', [CityController, 'getCity'])
+
+  }).prefix('/city')
+
+}).prefix('/api/v1')
