40e31ed93accfc1be85b87c82f6841ab0ff0a807
Joris VILARDELL <73846641+ZUHOWKS@users.noreply.github.com>
2025-07-02 17:45:30 +0200

    Update api.spec.ts

--- tests/functional/api.spec.ts
+++ tests/functional/api.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@japa/runner'
 
 test.group('City API endpoints', () => {
   test('Health check endpoint returns 204', async ({ client }) => {
-    const response = await client.get('/api/v1/_health')
+    const response = await client.get('/api/v1/health')
     response.assertStatus(204)
   })
 })
