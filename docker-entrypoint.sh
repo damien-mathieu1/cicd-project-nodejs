68b4216badd6db347021e436fe51c84b95784511
Damien MATHIEU <damien.mathieu@etu.umontpellier.fr>
2025-10-16 15:03:39 +0200

    remove seed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@
 echo "Running database migrations..."
 yes | node ace migration:run
 
-# Run seeds with force flag for production
-echo "Running database seeds..."
-yes | node ace db:seed
+# # Run seeds with force flag for production
+# echo "Running database seeds..."
+# yes | node ace db:seed
 
 # Start the server
 echo "Starting the server..."
